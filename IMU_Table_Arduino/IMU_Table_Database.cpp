3e3f71fee4a266ec622a6365cf16fa68291f4009
JLReitz <jlreitz@live.com>
2019-04-16 15:41:48 -0700

    no message

--- IMU_Table_Arduino/IMU_Table_Database.cpp
+++ IMU_Table_Arduino/IMU_Table_Database.cpp
@@ -9,9 +9,16 @@ voud IMU_TABLE_DATABASE::Update()
     {
         rx = ArduinoMailbox::Get_RX();
 
+        //Sequence number and hashcompare
+        nSequenceNum = 
+
+        //RX Status bits
         boperation_En_Cmd = rx.Operation_En();
         bMotor_x_En_Cmd = rx.Motor_x_En();
         bMotor_y_En_Cmd = rx.Motor_y_En();
         bMotor_z_En_Cmd = rx.Motor_z_En();
+
+        //Position command data
+
     }
 }
